a71a2a3b4636f00ded0c046e32e75b183691d548
Taras_Hrynchuk <Taras_Hrynchuk@epam.com>
2016-11-22 21:06:22 +0200

    heroes 1 final

--- angular2-tour-of-heroes/app/main.js
+++ angular2-tour-of-heroes/app/main.js
@@ -1,8 +1,7 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 /*
 Copyright 2016 Google Inc. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
