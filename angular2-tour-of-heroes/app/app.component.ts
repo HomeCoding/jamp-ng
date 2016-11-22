b17752df10e3606e87d551f39505f988234c359c
taras <tgrynchuk@gmail.com>
2016-11-22 16:58:08 +0200

    Revert "delete codebase"
    This reverts commit 065dd66b38a19adfaee52c793dea8e76149f7aae.
    
    # Conflicts:
    #	angular2-tour-of-heroes/app/app.component.js
    #	angular2-tour-of-heroes/app/app.component.js.map
    #	angular2-tour-of-heroes/app/app.component.ts
    #	angular2-tour-of-heroes/index.html
    #	angular2-tour-of-heroes/package.json
    #	angular2-tour-of-heroes/tsconfig.json

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,14 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from 'angular2/core';
 
 @Component({
-  selector: 'my-app',
-  template: '<h1>Hello Angular!</h1>'
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppComponent { }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
