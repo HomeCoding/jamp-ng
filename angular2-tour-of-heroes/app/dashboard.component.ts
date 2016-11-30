eb0c006a7051df245047759ef6bbfece3b11b82d
taras <tgrynchuk@gmail.com>
2016-11-30 15:53:18 +0200

    finish step 6

--- angular2-tour-of-heroes/app/dashboard.component.ts
+++ angular2-tour-of-heroes/app/dashboard.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
-    selector: 'my-dashboard',
-    template: '<h3>My Dashboard</h3>'
+  moduleId: module.id,
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard.component.html',
+  styleUrls: [ 'dashboard.component.css' ]
 })
+export class DashboardComponent implements OnInit {
 
-export class DashboardComponent{}
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
